2bf2da43bb43300cf81580ab41d541b4425b19c6
HeeFlower5001 <gmltmd3tp@naver.com>
2025-05-18 20:33:14 +0900

    create User

---
+++ Spring/demo/src/main/java/com/example/model/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String password;
+    private String nickname;
+    private LocalDate birthDate;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
