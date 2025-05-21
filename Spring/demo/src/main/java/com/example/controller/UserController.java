5dc19918ce56dfeb96e8d50769da33389e58d982
HeeFlower5001 <gmltmd3tp@naver.com>
2025-05-22 01:27:53 +0900

    2025_05_22

--- Spring/demo/src/main/java/com/example/controller/UserController.java
+++ Spring/demo/src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.User;
 import com.example.repository.UserRepository;
 
 @RestController
@@ -23,33 +21,4 @@ public class UserController {
 
         repository.save(user);
     }
-
-    @PostMapping("/login")
-    void login(@RequestBody String id, @RequestBody String pw) {
-        // 예외 처리 추가
-        return repository.find(id, pw);
-    }
-
-    @GetMapping("/User/{id}")
-    User getUser(@PathVariable String id) {
-        // 찾기 로직 바꾸기
-        return repository.findById(id);
-    }
-
-    // 비밀번호 변경
-    // 로그아웃
-
-    @DeleteMapping("/User/{id}")
-    void deleteUser(@PathVariable String id) {
-        // 삭제 로직 바꾸기
-        repository.deleteById(id);
-    }
-
-    // TTS 설정 조회
-    // TTS 설정 변경
-    // 알림 설정 조회
-    // 알림 설정 변경
-    // 주요 질문 조회
-    // 문의하기 전송
-    // 애플리케이션 정보 조회회
 }
