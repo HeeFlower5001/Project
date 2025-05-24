bb22ba4c6bfdb7d07bd2b1332a427ad65865b97b
Hee Seung Kim <gmltmd3tp@naver.com>
2025-05-24 17:46:58 +0900

    Initial commit

---
+++ Spring/CourseRegister/src/test/java/com/kau/network/CourseRegister/CourseRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kau.network.CourseRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
